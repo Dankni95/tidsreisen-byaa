d8a4827aa7124cfd1f6f3e02e32d34acf2fbd8e8
Daniel Lysak <Daniel_kn@live.no>
2022-05-10 10:37:27 +0200

    add server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express"
+import * as path from "path"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+import {WebSocketServer} from "ws";
+
+dotenv.config()
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cookieParser(process.env.COOKIE_SECRET))
+
+app.use(express.static("../client/build/"))
+
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        res.sendFile(path.resolve("../client/build/index.html"))
+    } else {
+        next()
+    }
+})
+
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`http://localhost:${server.address().port}`);
+});
