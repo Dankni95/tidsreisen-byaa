3119a08185cc50ed7aae488438a0317006a3e6bf
Daniel Lysak <Daniel_kn@live.no>
2022-05-14 02:47:39 +0200

    Cache

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,13 +5,13 @@ import { Application } from "./application.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
+
 navigator.serviceWorker.register(
     new URL('service-worker.js', import.meta.url),
-    {type: 'module'}
-).then(r =>{
+    { type: 'module' }
+).then(r => {
     console.log(r)
 });
-
 
 
 
