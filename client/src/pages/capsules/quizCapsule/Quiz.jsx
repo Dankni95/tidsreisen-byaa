95d3e6344f57ef1617da770b73c9292d50a03c90
Joachim AC <joacchri@gmail.com>
2022-05-16 11:43:22 +0200

    refactored quiz, less complex and works better

--- client/src/pages/capsules/quizCapsule/Quiz.jsx
+++ client/src/pages/capsules/quizCapsule/Quiz.jsx
@@ -1,8 +1,12 @@
 import {fetchJSON} from "../../../helpers/http.jsx";
 import {useLoading} from "../../../helpers/useLoading.jsx";
-import {QuizCard} from "./QuizCard.jsx";
+import {useState, react} from "react";
 
 export function Quiz() {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showPoints, setShowPoints] = useState(false);
+    const [points, setPoints] = useState(0);
+
     async function listQuiz() {
         return await fetchJSON("/api/quiz")
     }
@@ -24,9 +28,42 @@ export function Quiz() {
         );
     }
 
+    function handleAnswerClick(isCorrect) {
+        console.log(isCorrect)
+        if (isCorrect) {
+            setPoints(points + 1);
+        }
+
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < data.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowPoints(true);
+        }
+    }
+
     return (
-        <div style={{display: "grid", placeItems: "center"}}>
-            <QuizCard data={data} />
-        </div>
+        <main style={{display: "grid", placeItems: "center"}}>
+            {showPoints ?
+                (<div>
+                    <h1>Fullført quizkapsel</h1>
+                    <h3>Du har {points}/{data.length} riktige</h3>
+                    <a href={"/"}>Finn flere </a>
+                    <a href={"/"}>Mine funn</a>
+                </div>) : (
+                    <section>
+                        <div>
+                            <h1>Quizkapsel</h1>
+                            <h3>Sagtuft</h3>
+                            <p>{data[currentQuestion].question_}</p>
+                        </div>
+                        <div>
+                            {data[currentQuestion].answers.map((a, index) => (
+                                <button key={index} onClick={() => handleAnswerClick(a.isCorrect)}>{a.answer}</button>
+                            ))}
+                        </div>
+                    </section>
+                )}
+        </main>
     );
 }
--- client/src/pages/capsules/quizCapsule/QuizResult.jsx
+++
@@ -1,7 +0,0 @@
-export function QuizResult({points}) {
-    return (
-        <div>
-            <h1>points: {points}</h1>
-        </div>
-    );
-}
