9c333679cbe9b4f5860282bfe2f7726c2268a525
StianOek <66445281+StianOek@users.noreply.github.com>
2022-05-19 12:33:47 +0200

    Play && Pause does now work fine

--- client/src/pages/capsules/SoundCapsule/Play.jsx
+++ client/src/pages/capsules/SoundCapsule/Play.jsx
@@ -1,35 +1,50 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { useLoading } from "../../../helpers/useLoading";
 import { fetchJSON } from "../../../helpers/http";
+import { FaPlay } from "react-icons/fa";
+import { FaStop } from "react-icons/fa";
+import { FaPause } from "react-icons/fa";
+import { AiOutlinePause } from "react-icons/ai";
 
-let audioUrl = require("./Sound/dovregubben.mp3");
-const audio = new Audio(audioUrl);
 const Play = () => {
-  const { data, error, loading, reload } = useLoading(() =>
+  const audio = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  let audioUrl = require("./Sound/dovregubben.mp3");
+  /*  const { data, error, loading, reload } = useLoading(() =>
     fetchJSON("/api/sound")
-  );
-  if (loading) {
+  ); */
+  /*   if (loading) {
     return <h1>Loading..</h1>;
-  }
+  } */
 
-  var snd = new Audio("data:audio/wav;base64," + data[0].sound);
-  console.log(snd);
-  const test = () => {
-    audio.play();
+  /* var snd = new Audio("data:audio/wav;base64," + data[0].sound); */
 
-    console.log("playing");
+  /* const audio = new Audio(audioUrl); */
+  const handlePlay = () => {
+    if (isPlaying) {
+      setIsPlaying(!isPlaying);
+      audio.current.pause();
+    } else {
+      setIsPlaying(!isPlaying);
+      audio.current.play();
+    }
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <button onClick={test}>Testsadasdasdasdfgdghjhjkhjk</button>
-      <button style={{ cursor: "pointer" }}>Test2</button>
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <div className="d-flex justify-content-center align-items-center ">
+        <p>Start time</p>
+        <input type="range" />
+        <p>End time</p>
+      </div>
+      <div>
+        {isPlaying ? (
+          <FaPause onClick={handlePlay} size={25} />
+        ) : (
+          <FaPlay onClick={handlePlay} size={25} />
+        )}
+      </div>
+      <audio ref={audio} src={audioUrl}></audio>
     </div>
   );
 };
