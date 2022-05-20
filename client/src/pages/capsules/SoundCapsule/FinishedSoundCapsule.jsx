2c442d667b8979ca1ec6c0440fd7a29dc60c6e04
StianOek <66445281+StianOek@users.noreply.github.com>
2022-05-20 13:46:28 +0200

    last update - rendering finished component when currentTime reach end

---
+++ client/src/pages/capsules/SoundCapsule/FinishedSoundCapsule.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { CapsuleButton } from "../../../components/CapsuleButton";
+const FinishedSoundCapsule = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center flex-column vh-100 bg-capsule">
+      <h3 className="text-capsule fw-bold">Fullført lydkapselen</h3>
+      <h4>+20 poeng</h4>
+      <Link to="/">
+        <button>Finn flere</button>
+      </Link>
+    </div>
+  );
+};
+
+export default FinishedSoundCapsule;
--- client/src/pages/capsules/SoundCapsule/Sound.jsx
+++ client/src/pages/capsules/SoundCapsule/Sound.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Play from "./Play";
 import sawmillwork from "../../../images/soundcapsule/image1.svg";
+import { FaItunesNote } from "react-icons/fa";
+import FinishedSoundCapsule from "./FinishedSoundCapsule";
 const Sound = () => {
+  const [drag, setDrag] = useState();
+  const [songInfo, setSongInfo] = useState({
+    currentTime: null,
+    duration: null,
+  });
+  console.log(songInfo.currentTime);
+  if (songInfo.currentTime >= songInfo.duration) {
+    return <FinishedSoundCapsule />;
+  }
+
   return (
-    <div className="pt-5 d-flex justify-content-center align-items-center flex-column">
-      <h3 className="pb-3">Lydkapsel</h3>
-      <h1 className="pb-3 fw-bolder text-color-capsule">Byåa</h1>
-      <img className="p-2" src={sawmillwork} alt="" srcset="" />
-      <Play />
-    </div>
+    <>
+      <div className=" d-flex justify-content-center align-items-center flex-column vh-100 bg-capsule">
+        <div className="position-absolute top-4 index">
+          <FaItunesNote size={300} />
+        </div>
+
+        <h3 className="pb-3">Lydkapsel</h3>
+        <h1 className="pb-3 fw-bolder text-capsule">Byåa</h1>
+        <img className="p-2" src={sawmillwork} alt="" srcset="" />
+        <Play songInfo={songInfo} setSongInfo={setSongInfo} setDrag={setDrag} />
+        <h4 className="my-5 fw-bold">Sagbruket - År 1700-1800</h4>
+        <p className="fst-italic fw-bold">
+          Lytt ferdig lydlkapselen for å få poeng
+        </p>
+      </div>
+    </>
   );
 };
 
