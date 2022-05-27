7c4ef280c02bd6ddcee69c3904a8436cb93da11f
Rune Jacobsen <69790920+runejac@users.noreply.github.com>
2022-05-27 15:31:22 +0200

    My findings logic (#63)
    * setting up the logic, not done
    
    * MyFindings.jsx now shows the users capsules done. Next: fix so it will be unique / check if user have done the capsule on the capsules page. Also figure out how to show correct image.
    
    * Working on HISTORY capsule: passing correct capsule and checks if it exists from before, can not add duplicates. BUT: if on the same render, they can swipe left and right and it gets added because user state is not updated so working on that next.
    
    * Only unique capsules can be pushed to user db now, checking for $ne backend.
    
    * new message on last slide if its done
    
    * last slide shows correct IF user slides one time to the left and to the end again
    
    * myfindings fix style
    
    Co-authored-by: Joachim Christensen <46721078+joachr@users.noreply.github.com>

--- client/src/pages/MyFindings.jsx
+++ client/src/pages/MyFindings.jsx
@@ -3,7 +3,7 @@ import { MyFindingsCard } from "../components/MyFindingsCard.jsx";
 
 export function MyFindings() {
   return (
-    <div className={"container"}>
+    <div className={"container"} id={"container"}>
       <MyFindingsCard />
     </div>
   );
