5a9532e4cc2f17636f25331e1919a262f1224efe
Daniel Lysak <Daniel_kn@live.no>
2022-05-27 08:37:33 +0200

    Add promt to update cache (#58)

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,11 +4,49 @@ import "./index.css";
 import { Application } from "./application.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// *** PWA Functionality START ***
+
 navigator.serviceWorker
   .register(new URL("service-worker.js", import.meta.url), { type: "module" })
-  .then((r) => {
-    console.log(r);
-  });
+  .then((reg) => {
+    reg.onupdatefound = () => {
+      const installingWorker = reg.installing;
+      installingWorker.onstatechange = () => {
+        switch (installingWorker.state) {
+          case "installed":
+            if (navigator.serviceWorker.controller) {
+              // new update available
+              console.log("new update available");
+
+              promptUserToRefresh().then((r) => {
+                window.location.reload(true);
+                console.log("updated");
+              });
+            } else {
+              // no update available
+              console.log("No updates");
+            }
+            break;
+        }
+      };
+    };
+  })
+  .catch((err) => console.error("[SW ERROR]", err));
+
+async function promptUserToRefresh() {
+  const registration = await navigator.serviceWorker.getRegistration();
+  registration.waiting.postMessage("SKIP_WAITING");
+
+  /*
+  skip asking to update when developing
+  if (window.confirm("New version available! Refresh?")) {
+    registration.waiting.postMessage("SKIP_WAITING");
+  }
+
+   */
+}
+
+// *** PWA Functionality END *
 
 ReactDOM.render(
   <React.StrictMode>
