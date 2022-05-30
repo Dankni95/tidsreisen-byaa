8fd613ee985b4b6f0ad14056bc98c3a1cd646e16
runejac <69790920+runejac@users.noreply.github.com>
2022-05-30 22:26:18 +0200

    working on showing all capsules, not visited to be gray and working on image

--- client/src/components/MyFindingsCard.jsx
+++ client/src/components/MyFindingsCard.jsx
@@ -1,19 +1,30 @@
 import React, { useContext } from "react";
 import "../css/myFindingsCard.css";
-import kvernhus from "../assets/images/kvernhus3.png";
+import kvernhus from "../assets/images/myfindings-dummy-image.png";
 import { FiCheck } from "react-icons/fi";
 import { User } from "../application.jsx";
+import { MdQuiz } from "react-icons/md";
+import { FaBook } from "react-icons/fa";
+import { AiFillSound } from "react-icons/ai";
+import { HiOutlineCheckCircle } from "react-icons/hi";
+import { myFindingsCardData } from "./myFindingsCardData.jsx";
 
 export function MyFindingsCard() {
   const { user, setUser } = useContext(User);
+  // easier to handle potential issues with strings when in a variable
+  const HISTORYCAPSULE = "Historiekapsel";
+  const QUIZCAPSULE = "Quizkapsel";
+  const AUDIOCAPSULE = "Lydkapsel";
+
   /**
    * TODO: må finne ut av hvordan vi skal hente bilde og matche det med user.finishedCapsules.name
    * */
 
   return (
     <>
-      {user.finishedCapsules?.map((capsule, index) => {
+      {myFindingsCardData.finishedCapsules?.map((capsule, index) => {
         return (
+          /*<div id={"inner-container"} key={index}>*/
           <div
             id={"card"}
             className="card mb-3"
@@ -21,30 +32,44 @@ export function MyFindingsCard() {
             onClick={() => console.log("Opens capsule but not 'done'-page")}
             key={index}
           >
-            <div className="row g-0">
-              <div className="col-5">
+            <div id={"content-container"} className="row g-0">
+              <div id={"image-container"} className="col-5">
                 <img
-                  height={100}
+                  id={"image"}
                   src={kvernhus}
-                  className="img-fluid rounded-start"
+                  className="img-fluid"
                   alt="dummyalttext"
                 />
               </div>
-              <div className="col-5">
+              <div className="col-6">
                 <div className="card-body">
-
                   <h5 className="card-title">
                     {capsule.name || capsule.name_}
                   </h5>
 
-                  <p className="card-text">{capsule.category}</p>
+                  <p className="card-text">
+                    {capsule.category === HISTORYCAPSULE && (
+                      <FaBook color={"var(--textColorGray)"} />
+                    )}
+                    {capsule.category === QUIZCAPSULE && (
+                      <MdQuiz color={"var(--textColorGray)"} />
+                    )}
+                    {capsule.category === AUDIOCAPSULE && (
+                      <AiFillSound color={"var(--textColorGray)"} />
+                    )}
+                    {capsule.category}
+                  </p>
                 </div>
               </div>
-              <div className={"col-2"}>
-                <FiCheck size={50} />
+              <div id={"done-icon"} className={"col-1"}>
+                <HiOutlineCheckCircle
+                  color={"var(--textColorGray)"}
+                  size={40}
+                />
               </div>
             </div>
           </div>
+          /*</div>*/
         );
       })}
     </>
---
+++ client/src/components/myFindingsCardData.jsx
@@ -0,0 +1,38 @@
+// used for the
+
+export const myFindingsCardData = {
+  finishedCapsules: [
+    {
+      name: "Vannsag",
+      category: "Historiekapsel",
+    },
+    {
+      name: "Vannsag",
+      category: "Historiekapsel",
+    },
+    {
+      name: "Vannsag",
+      category: "Historiekapsel",
+    },
+    {
+      name: "Vannsag",
+      category: "Historiekapsel",
+    },
+    {
+      name: "Vannsag",
+      category: "Historiekapsel",
+    },
+    {
+      name: "Vannsag",
+      category: "Historiekapsel",
+    },
+    {
+      name: "Vannsag",
+      category: "Historiekapsel",
+    },
+    {
+      name: "Vannsag",
+      category: "Historiekapsel",
+    },
+  ],
+};
