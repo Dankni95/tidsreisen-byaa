48a34e26ffb8d908e31739190a746bc878d82446
Joachim Christensen <46721078+joachr@users.noreply.github.com>
2022-05-31 20:43:31 +0200

    Starting jest (#80)
    * started testing with jest
    
    * test "should add a user" passing
    
    * structure for further login tests, as well as history, quiz and sound tests, added
    
    * test "should get quiz capsule from database" passing
    
    * test "should get history capsule from database" passing
    
    * .env both in root and server
    
    * test "should get sound capsule from database" passing
    
    * small quiz fixes as discussed in meeting
    
    Co-authored-by: Joachim AC <joacchri@gmail.com>

---
+++ server/__tests__/soundApi.test.jsx
@@ -0,0 +1,54 @@
+import express from "express";
+import request from "supertest";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { SoundApi } from "../soundApi.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+
+const mongoDbClient = new MongoClient(process.env.MONGODB_URL);
+const database = mongoDbClient.db("test_db");
+
+beforeAll(async () => {
+    await mongoDbClient.connect().then(async () => {
+        console.log("Connected to MongoDB");
+        await database.collection("sound").insertOne({
+            title: "Test",
+            sound: "",
+            category: "Lydkapsel",
+            year: "år 2022",
+            image: ""
+        });
+        app.use("/api/sound", SoundApi(database));
+    });
+});
+afterAll(async () => {
+    await mongoDbClient.connect().then(async () => {
+        await database.collection("sound").deleteMany({});
+        app.use("/api/sound", SoundApi(database));
+    });
+    mongoDbClient.close();
+});
+
+describe("SoundApi", () => {
+    it('should get sound capsule from database', async () => {
+
+        const title = "Test"
+
+        expect(
+            (
+                await request(app).get("/api/sound/").expect(200)
+            ).body.map(({ title }) => title)
+        ).toContain(title);
+    });
+})
