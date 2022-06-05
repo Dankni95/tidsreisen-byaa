7548a3607a9530dbe4caea6ceb9f1560e6da8587
Joachim AC <joacchri@gmail.com>
2022-06-05 20:06:21 +0200

    notloggedin.test.jsx "simulates click" passing

--- client/src/pages/capsules/HistoryCapsule/StoryCard.jsx
+++ client/src/pages/capsules/HistoryCapsule/StoryCard.jsx
@@ -1,9 +1,18 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Loading } from "../../../components/Loading.jsx";
+// swiper bundle styles
+import "swiper/swiper-bundle.min.css";
+
+// swiper core styles
 import "swiper/swiper.min.css";
-import "swiper/swiper-bundle.css";
+
+// modules styles
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Scrollbar } from "swiper";
+import SwiperCore from "swiper";
 import { useParams } from "react-router-dom";
 import "./storycard.css";
 import { UserContext } from "../../../contexts/userContext.jsx";
@@ -13,6 +22,8 @@ import { ErrorModal } from "../../../components/ErrorModal.jsx";
 import { FaBook } from "react-icons/fa";
 
 export function StoryCard({ historyCapsule, error, loading }) {
+  SwiperCore.use([Scrollbar]);
+
   const { id } = useParams();
   const { updateUser } = useContext(UserContext);
   const { user, setUser } = useContext(User);
@@ -53,96 +64,95 @@ export function StoryCard({ historyCapsule, error, loading }) {
 
   /*const olafHiderLeft = () =>
     (document.querySelector("#olaf-left").style.display = "none");
-
   const olafDisplayerLeft = () =>
     (document.querySelector("#olaf-left").style.display = "block");*/
 
   if (historyCapsule.name.toLowerCase() === id.toLowerCase()) {
     return (
-      <div>
-        <div id={"olaf-left-div"}>
-          {/*<img
+        <div>
+          <div id={"olaf-left-div"}>
+            {/*<img
             id={"olaf-left"}
             src={olafLeft}
             alt="bilde av olaf på venstre side"
           />*/}
-        </div>
-        <div className={"p-4"} style={{ position: "relative", zIndex: "0" }}>
-          <div style={{ position: "relative" }}>
-            <h3
-              className={"text-center"}
-              style={{
-                color: "var(--textColorGray)",
-                fontFamily: "Source Sans Pro Semibold",
-              }}
-            >
-              <FaBook color={"var(--textColorGray)"} /> Historiekapsel
-            </h3>
-            <h1
-              className={"text-center"}
-              style={{
-                color: "var(--backgroundColorGreeny)",
-                fontFamily: "Source Sans Pro Bold",
-              }}
-            >
-              {historyCapsule.name}
-            </h1>
           </div>
-          <Swiper
-            modules={[Scrollbar]}
-            centeredSlides={true}
-            spaceBetween={50}
-            slidesPerView={1}
-            scrollbar={{ draggable: true }}
-            onReachEnd={async () => await updateToDatabase()}
-            onSwiper={(swiper) => console.log(swiper)}
-          >
-            {historyCapsule.story.map((capsuleStory, index) => {
-              return (
-                <SwiperSlide key={index} className={"slide"}>
-                  {capsuleStory.done === true ? (
-                    <HistoryDone
-                      updateToDatabase={updateToDatabase}
-                      id={historyCapsule.id}
-                    />
-                  ) : (
-                    <div className={"swiper-slide"} key={index}>
-                      <div className={"slide-content"}>
-                        <div>
-                          <div style={{ maxWidth: "45rem" }} className={"mt-5"}>
-                            {capsuleStory.image && (
-                              <section>
-                                <img
-                                  id={"history-image"}
-                                  className={"card-img"}
-                                  src={capsuleStory.image}
-                                  alt={"bilde av historie-element"}
-                                />
-                              </section>
-                            )}
+          <div className={"p-4"} style={{ position: "relative", zIndex: "0" }}>
+            <div style={{ position: "relative" }}>
+              <h3
+                  className={"text-center"}
+                  style={{
+                    color: "var(--textColorGray)",
+                    fontFamily: "Source Sans Pro Semibold",
+                  }}
+              >
+                <FaBook color={"var(--textColorGray)"} /> Historiekapsel
+              </h3>
+              <h1
+                  className={"text-center"}
+                  style={{
+                    color: "var(--backgroundColorGreeny)",
+                    fontFamily: "Source Sans Pro Bold",
+                  }}
+              >
+                {historyCapsule.name}
+              </h1>
+            </div>
+            <Swiper
+                centeredSlides={true}
+                spaceBetween={50}
+                slidesPerView={1}
+                scrollbar={{ draggable: true }}
+                onReachEnd={async () => await updateToDatabase()}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+              {historyCapsule.story.map((capsuleStory, index) => {
+                return (
+                    <SwiperSlide key={index} className={"slide"}>
+                      {capsuleStory.done === true ? (
+                          <HistoryDone
+                              updateToDatabase={updateToDatabase}
+                              id={historyCapsule.id}
+                          />
+                      ) : (
+                          <div className={"swiper-slide"} key={index}>
+                            <div className={"slide-content"}>
+                              <div>
+                                <div style={{ maxWidth: "45rem" }} className={"mt-5"}>
+                                  {capsuleStory.image && (
+                                      <section>
+                                        <img
+                                            id={"history-image"}
+                                            className={"card-img"}
+                                            src={capsuleStory.image}
+                                            alt={"bilde av historie-element"}
+                                        />
+                                      </section>
+                                  )}
+                                </div>
+
+                                <section id={"story-section"} className={"mt-4"}>
+                                  <p id={"story-paragraph"}>{capsuleStory.story}</p>
+                                </section>
+                              </div>
+                            </div>
+                            <div id={"year"}>
+                              <h4 className=" fw-bold">{capsuleStory.year}</h4>
+                            </div>
                           </div>
-                          <section id={"story-section"} className={"mt-4"}>
-                            <p id={"story-paragraph"}>{capsuleStory.story}</p>
-                          </section>
-                        </div>
-                        <div id={"year"}>
-                          <h4 className=" fw-bold">{capsuleStory.year}</h4>
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
+                      )}
+                    </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
         </div>
-      </div>
     );
   }
   return (
-    <>
-      {loading && <Loading />}
-      {error && <ErrorModal error={error} />}
-    </>
+      <>
+        {loading && <Loading />}
+        {error && <ErrorModal error={error} />}
+      </>
   );
 }
