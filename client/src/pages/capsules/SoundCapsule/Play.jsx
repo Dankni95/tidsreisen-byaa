4827854116a9732e91a830ab56038663e2a2fac5
StianOek <66445281+StianOek@users.noreply.github.com>
2022-06-06 19:20:59 +0200

    rydding av kode, kommentarer og struktur

--- client/src/pages/capsules/SoundCapsule/Play.jsx
+++ client/src/pages/capsules/SoundCapsule/Play.jsx
@@ -1,52 +1,49 @@
 import React, { useRef, useState } from "react";
 import { FaPause, FaPlay } from "react-icons/fa";
-import vegfar from "./Sound/vegfar.mp3";
-import vesledammen from "./Sound/vesledammen.mp3";
-import vannsag from "./Sound/vannsag.mp3";
-import twoSecSoundFile from "./Sound/2-seconds-of-silence.mp3";
+import vegfar from "../../../assets/Sound/vegfar.mp3";
+import vesledammen from "../../../assets/Sound/vesledammen.mp3";
+import vannsag from "../../../assets/Sound/vannsag.mp3";
+
+// Test audio file for shorter duration
+import twoSecSoundFile from "../../../assets/Sound/2-seconds-of-silence.mp3";
 import { useParams } from "react-router-dom";
 import "../../../css/sound.css";
+
 const Play = ({ songInfo, setSongInfo, setDrag }) => {
   const { id } = useParams();
-
-  const audio = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const audio = useRef(null);
 
   let audioUrl = "";
 
-  // FIXME, using 2seconds sound file ONLY for test purposes
+  // Check si that the right audio file is playing in the right capsule
   if (id === "vegfar") {
     //audioUrl = vegfar;
-    audioUrl = twoSecSoundFile;
+    audioUrl = vegfar;
   }
   if (id === "vannsag") {
     //audioUrl = vannsag;
-    audioUrl = twoSecSoundFile;
+    audioUrl = vannsag;
   }
   if (id === "vesledammen") {
     //audioUrl = vesledammen;
-    audioUrl = twoSecSoundFile;
+    audioUrl = vesledammen;
   }
 
-  /*  const { data, error, loading, reload } = useLoading(() =>
-    fetchJSON("/api/sound")
-  ); */
-  /*   if (loading) {
-    return <h1>Loading..</h1>;
-  } */
-
+  // Setting current time and duration in state
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
     setSongInfo({ ...songInfo, currentTime: current, duration: duration });
   };
 
+  // Formatting current time and duration of the audio file
   const getTime = (time) => {
     return (
       Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
     );
   };
-
+  // Check if audio file is playing or not
   const handlePlay = () => {
     if (isPlaying) {
       setIsPlaying(!isPlaying);
@@ -56,7 +53,7 @@ const Play = ({ songInfo, setSongInfo, setDrag }) => {
       audio.current.play();
     }
   };
-
+  // Updates time in realtime, so that the input range is changing according to audio's current time
   const onDragHandler = (e) => {
     setDrag(e.target.value);
   };
