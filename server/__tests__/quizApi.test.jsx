9e9279f927172f2c80d4092dd12f3961343824a0
runejac <69790920+runejac@users.noreply.github.com>
2022-06-06 16:46:31 +0200

    changed some ordering and removed some .then things on server api tests, work now?

--- server/__tests__/quizApi.test.jsx
+++ server/__tests__/quizApi.test.jsx
@@ -5,10 +5,8 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import { QuizApi } from "../quizApi.js";
 
-dotenv.config();
-
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -17,28 +15,27 @@ app.use(
 );
 
 const mongoDbClient = new MongoClient(process.env.MONGODB_URL);
-const database = mongoDbClient.db("test_db");
 
 beforeAll(async () => {
-  await mongoDbClient.connect().then(async () => {
-    console.log("Connected to MongoDB");
-    await database.collection("quiz").insertOne({
-      category: "Quizkapsel",
-      question_: "Hva er en test?",
-      name_: "Test-quiz",
-      answers: [
-        {
-          answer: "Dette er en test",
-          isCorrect: true,
-        },
-        {
-          answer: "Dette er ikke en test",
-          isCorrect: false,
-        },
-      ],
-    });
-    app.use("/api/quiz", QuizApi(database));
+  await mongoDbClient.connect();
+  const database = mongoDbClient.db("test_db");
+  console.log("Connected to MongoDB");
+  await database.collection("quiz").insertOne({
+    category: "Quizkapsel",
+    question_: "Hva er en test?",
+    name_: "Test-quiz",
+    answers: [
+      {
+        answer: "Dette er en test",
+        isCorrect: true,
+      },
+      {
+        answer: "Dette er ikke en test",
+        isCorrect: false,
+      },
+    ],
   });
+  app.use("/api/quiz", QuizApi(database));
 });
 afterAll(async () => {
   await mongoDbClient.connect().then(async () => {
--- server/__tests__/soundApi.test.jsx
+++ server/__tests__/soundApi.test.jsx
@@ -5,10 +5,8 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import { SoundApi } from "../soundApi.js";
 
-dotenv.config();
-
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -17,20 +15,19 @@ app.use(
 );
 
 const mongoDbClient = new MongoClient(process.env.MONGODB_URL);
-const database = mongoDbClient.db("test_db");
 
 beforeAll(async () => {
-  await mongoDbClient.connect().then(async () => {
-    console.log("Connected to MongoDB");
-    await database.collection("sound").insertOne({
-      title: "Test",
-      sound: "",
-      category: "Lydkapsel",
-      year: "år 2022",
-      image: "",
-    });
-    app.use("/api/sound", SoundApi(database));
+  await mongoDbClient.connect();
+  const database = mongoDbClient.db("test_db");
+  console.log("Connected to MongoDB");
+  await database.collection("sound").insertOne({
+    title: "Test",
+    sound: "",
+    category: "Lydkapsel",
+    year: "år 2022",
+    image: "",
   });
+  app.use("/api/sound", SoundApi(database));
 });
 afterAll(async () => {
   await mongoDbClient.connect().then(async () => {
